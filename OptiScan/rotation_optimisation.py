94702171f35a337e5defac465d0df6a04f4358b7
Akdel <mehmet_akdel@hotmail.com>
2018-11-29 15:10:01 +0100

    saphyr args

--- OptiScan/rotation_optimisation.py
+++ OptiScan/rotation_optimisation.py
@@ -416,13 +416,13 @@ def get_yshift2(top_image_bottom, bottom_image_top, return_score=False):
 
 def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
     import matplotlib.pyplot as plt
-    # if saphyr:
-    #     filtered_pairs = [(top_image_bottom[:,i], bottom_image_top[:,i]) for i in range(0, top_image_bottom.shape[1], 1)]
-    # else:
     pairs = [(top_image_bottom[:,i], bottom_image_top[:,i]) for i in range(0, top_image_bottom.shape[1], 1)]
     xmed = np.median([sum(x) for x, y in pairs])
     ymed = np.median([sum(y) for x, y in pairs])
-    filtered_pairs = [(x,y) for x, y in pairs if (sum(x)/1.5 >= xmed) or (sum(y)/1.5 >= ymed)]
+    if saphyr:
+        filtered_pairs = [(x,y) for x, y in pairs if (sum(x) >= xmed * 3) or (sum(y) >= ymed * 3)]
+    else:
+        filtered_pairs = [(x,y) for x, y in pairs if (sum(x)/1.5 >= xmed) or (sum(y)/1.5 >= ymed)]
     corrs = np.array([np.correlate(y, x, mode="full") for x, y in filtered_pairs], dtype=float)
     corr_sum = np.sum(corrs, axis=0)
     if debug:
