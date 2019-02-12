eb154b300cd166396f1884d823837bc412ee5f43
Akdel <mehmet_akdel@hotmail.com>
2019-02-12 09:55:18 +0100

    debug print

--- OptiScan/rotation_optimisation.py
+++ OptiScan/rotation_optimisation.py
@@ -141,6 +141,7 @@ def get_optimal_rotation(image, _from=-0.1, _to=0.1, initial_space=0.1, final_sp
 
 
 def rotate_with_optimal_rotation(image, _from=-0.1, _to=0.1, initial_space=0.05, final_space=0.05, saphyr=False):
+    print(True, _from, _to, initial_space, final_space)
     angle = max(get_optimal_rotation(image, _from=_from, _to=_to,
                                      initial_space=initial_space, final_space=final_space, saphyr=saphyr))[1]
     return ndimage.rotate(image, angle, reshape=False), angle
