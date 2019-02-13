9fc4748184248b25725aca6320a4fac63eab0f68
Akdel <mehmet_akdel@hotmail.com>
2019-02-13 17:06:59 +0100

    debug false

--- OptiScan/rotation_optimisation.py
+++ OptiScan/rotation_optimisation.py
@@ -417,7 +417,7 @@ def get_yshift2(top_image_bottom, bottom_image_top, return_score=False):
     return np.argmax(corr_sum[:60])
 
 
-def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
+def get_yshift(top_image_bottom, bottom_image_top, debug=False, saphyr=False):
     pairs = [(top_image_bottom[:,i], bottom_image_top[:,i]) for i in range(0, top_image_bottom.shape[1], 1)]
     xmed = np.median([sum(x) for x, y in pairs])
     ymed = np.median([sum(y) for x, y in pairs])
