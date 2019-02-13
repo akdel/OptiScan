7edb548cbd7ef3789636e9aaae6c5b10214e1f6d
Akdel <mehmet_akdel@hotmail.com>
2019-02-13 12:47:20 +0100

    debug mode off

--- OptiScan/rotation_optimisation.py
+++ OptiScan/rotation_optimisation.py
@@ -413,7 +413,7 @@ def get_yshift2(top_image_bottom, bottom_image_top, return_score=False):
     return np.argmax(corr_sum[:60])
 
 
-def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
+def get_yshift(top_image_bottom, bottom_image_top, debug=False, saphyr=False):
     pairs = [(top_image_bottom[:,i], bottom_image_top[:,i]) for i in range(0, top_image_bottom.shape[1], 1)]
     xmed = np.median([sum(x) for x, y in pairs])
     ymed = np.median([sum(y) for x, y in pairs])
@@ -424,7 +424,7 @@ def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
         filtered_pairs = [(x,y) for x, y in pairs if (sum(x) >= xmed * 2) or (sum(y) >= ymed * 2)]
         corrs = np.array([ncorr(y, x, limit=12) for x, y in filtered_pairs], dtype=float)
     if len(filtered_pairs) == 0:
-        return 0.0
+        return 0
     corr_sum = np.sum(corrs, axis=0)
     if debug:
         plt.imshow(top_image_bottom)
@@ -441,8 +441,6 @@ def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
     if saphyr:
         return np.argmax(corr_sum[:300])
     else:
-        print(corr_sum)
-        print(corr_sum.shape)
         return np.argmax(corr_sum[:60])
 
 
