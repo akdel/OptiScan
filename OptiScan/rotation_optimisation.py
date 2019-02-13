f23555f9f845a77e0795b4c40ecb652e8da3c9f7
Akdel <mehmet_akdel@hotmail.com>
2019-02-13 16:39:39 +0100

    hardparam change

--- OptiScan/rotation_optimisation.py
+++ OptiScan/rotation_optimisation.py
@@ -425,7 +425,7 @@ def get_yshift(top_image_bottom, bottom_image_top, debug=True, saphyr=False):
         filtered_pairs = [(x,y) for x, y in pairs if (sum(x) >= xmed * 4) or (sum(y) >= ymed * 4)]
         corrs = np.array([ncorr(y, x, limit=25) for x, y in filtered_pairs], dtype=float)
     else:
-        filtered_pairs = [(x,y) for x, y in pairs if (sum(x) >= xmed * 100) or (sum(y) >= ymed * 100)]
+        filtered_pairs = [(x,y) for x, y in pairs if (sum(x) >= xmed * 25) or (sum(y) >= ymed * 25)]
         corrs = np.array([ncorr(y, x, limit=8) for x, y in filtered_pairs], dtype=float) #limit=12
     if len(filtered_pairs) == 0:
         return 0
