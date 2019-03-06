0553176be3bd8a634572d2e1e26e5ffce7a33a7a
Akdel <mehmet_akdel@hotmail.com>
2019-03-06 11:19:57 +0100

    fixed arguments for refaligner

--- OptiScan/utils.py
+++ OptiScan/utils.py
@@ -780,17 +780,16 @@ class MQR:
         self.ref_align = ref_align_path
         self.output_dir = output_folder
         self.command = """%s -f -ref %s -i %s -o %s -nosplit 2 -BestRef 1 -biaswt 0 -Mfast 0 -FP 1.5 -FN 0.05 \
-            -sf 0.2 -sd 0.0 -A 5 -outlier 1e-3 -outlierMax 40 -endoutlier 1e-4 -S -1000 -sr 0.03 -se 0.2 -MaxSF\
+            -sf 0.2 -sd 0.0 -A 5 -outlier 1e-3 -outlierMax 40 -endoutlier 1e-4 -S -1000 -sr 0.03 -se 0.2 -MaxSF \
              0.3 -MaxSE 0.5 -MaxSD 0.12 -resbias 4 64 -maxmem 64 -M 3 3 -minlen 50  -T %s -maxthreads 12 \
              -hashgen 5 3 2.4 1.5 0.05 5.0 1 1 3 -hash -hashdelta 10 -hashoffset 1 -hashmaxmem 64 -insertThreads 4 \
              -maptype 0 -PVres 2 -PVendoutlier -AlignRes 2.0 -rres 0.9 -resEstimate -ScanScaling 2 -RepeatMask 5 0.01 \
              -RepeatRec 0.7 0.6 1.4 -maxEnd 50 -usecolor 1 -stdout -stderr -subset 1 %s"""
-        self.pairwise_command = """%s -f -i %s -o %s -nosplit 2 -BestRef 1 -biaswt 0 -Mfast 0 -FP 1.5 -FN 0.05 -align_format 0\
-            -sf 0.2 -sd 0.0 -A 5 -outlier 1e-3 -outlierMax 40 -endoutlier 1e-4 -S -1000 -sr 0.03 -se 0.2 -MaxSF\
-             0.3 -MaxSE 0.5 -MaxSD 0.12 -resbias 4 64 -maxmem 64 -M 3 3 -minlen 50  -T %s -maxthreads 12 \
-             -hashgen 5 3 2.4 1.5 0.05 5.0 1 1 3 -hash -hashdelta 10 -hashoffset 1 -hashmaxmem 64 -insertThreads 4 \
-             -maptype 0 -PVres 2 -PVendoutlier -AlignRes 2.0 -rres 0.9 -resEstimate -ScanScaling 2 -RepeatMask 5 0.01 \
-             -RepeatRec 0.7 0.6 1.4 -maxEnd 50 -usecolor 1 -stdout -stderr -subset 1 %s"""
+        self.pairwise_command = """%s -i %s -o %s -usecolor 1 -FP 1.5 -FN 0.15 -sd 0.0 -sf 0.2 -sr 0.03 -res 3.3 -T %s -maxmem 7.5 \
+            -minlen 150 -minsites 8 -MaxIntensity 20000 -usecolor 1 -maxsites 200 -mres 0.9 -usecolor 1 -A 5 \
+            -S 1 -MaxSE 0.5 -outlier 0.0001 -outlierMax 40. -endoutlier 0 -RepeatMask 2 0.01 -RepeatRec 0.7 0.6 1.4 -PVres 2 \
+            -alignscore -maptype 0 -HSDrange 1.0 -hashoffset 1 -f -hashgen 5 3 2.2 1.2 0.05 3.0 1 1 1 -hash \
+            -nosplit 2 -align_format 0 -stdout -stderr -maxthreads 24"""
         self.xmap = None
 
     def run_ref_align(self, reference_cmap_path, bnx_file, number_of_molecules):
@@ -805,3 +804,10 @@ class MQR:
         xmap_file = self.output_dir + 'bnx_quality.xmap'
         self.xmap = XmapParser(xmap_file)
 
+
+
+"""%s -i %s -o %s -usecolor 1 -FP 1.5 -FN 0.15 -sd 0.0 -sf 0.2 -sr 0.03 -res 3.3 -T %s -maxmem 7.5 \
+    -minlen 150 -minsites 8 -MaxIntensity 20000 -usecolor 1 -maxsites 200 -mres 0.9 -usecolor 1 -A 5 \
+    -S 1 -MaxSE 0.5 -outlier 0.0001 -outlierMax 40. -endoutlier 0 -RepeatMask 2 0.01 -RepeatRec 0.7 0.6 1.4 -PVres 2\
+     -alignscore -maptype 0 -HSDrange 1.0 -hashoffset 1 -f -hashgen 5 3 2.2 1.2 0.05 3.0 1 1 1 -hash \
+    -nosplit 2 -align_format 0 -stdout -stderr -maxthreads 24"""
